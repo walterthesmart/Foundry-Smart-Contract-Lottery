98a4ec03ee1230daeaf03a8c44a68b98caf69f08
walterthesmart <nwaugowalter@gmail.com>
2024-04-14 04:21:31 +0100

    fund subscrption script

--- script/Interactions.s.sol
+++ script/Interactions.s.sol
@@ -18,11 +18,11 @@ contract CreateSubscription is Script {
     function createSubscription(address vrfCoordinator) public returns(uint6){
         console.log("Creating a subscription on chainId: ", block.chainid);
         vm.startBroadcast();
-        uint64 subId = VRFCoordinatorV2Mock(vrfCoordinator).createSubscription();
+        uint64 subscriptionId = VRFCoordinatorV2Mock(vrfCoordinator).createSubscription();
         vm.stopBroadcast();
-        console.log("Subscription created with id: ", subId);
+        console.log("Subscription created with id: ", subscriptionId);
         console.log("Please update subscription id in HelperConfig.s.sol");
-        return subId;
+        return subscriptionId;
     }
 
     function run() external returns(uint64) {
@@ -38,6 +38,24 @@ contract FundSubscription is Script {
         // Fund a subscription
         HelperConfig helperConfig = new HelperConfig();
         (, , address vrfCoordinator, , uint64 subscriptionId,address linkToken) = helperConfig.activeNetworkConfig();
-        fundSubscription(vrfCoordinator, subscriptionId);
+        fundSubscription(vrfCoordinator, subscriptionId, linkToken);
+    }
+
+    function fundSubscription(address vrfCoordinator, uint64 subscriptionId, address linkToken) public {
+        console.log("Funding subscription: ", subscriptionId);
+        console.log("Using vrfCoordinator: ", vrfCoordinator);
+        console.log("Funding subscription on chainId: ", block.chainid);
+        if(block.chainid == 11155111 || block.chainid == 97 || block.chainid == 43113 || block.chainid == 421614 || block.chainid == 4002){
+            console.log("Funding on an actual testnet blockchain");
+            vm.startBroadcast();
+            VRFCoordinatorV2Mock(vrfCoordinator).fundSubscription(subscriptionId, FUND_AMOUNT);
+            vm.stopBroadcast();
+        }
+        else {
+            console.log("Funding on a local blockchain");
+            vm.startBroadcast();
+            LinkTokenInterface(linkToken).transferAndCall(vrfCoordinator, FUND_AMOUNT, abi.encode(subscriptionId));
+        }
+        console.log("Subscription funded with: ", FUND_AMOUNT);
     }
 }
